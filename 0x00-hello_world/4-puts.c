d4b3f79b2737aff08be1104195ffc22e49ebcd2a
Ruthlessness <Nwaforbella@gmail.com>
2023-06-25 19:13:42 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *main -it prints out using put
+ *main - it prints out using put
  *
- * Return always (0)
+ * Return: 0 (success)
  *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
