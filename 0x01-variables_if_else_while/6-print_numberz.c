c947db910a47beb81bf03aa1ae63d12a0f5922eb
Ruthlessness <Nwaforbella@gmail.com>
2023-06-29 08:36:32 -0700

    tadk6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int b = '0';
+
+	while (b <= '9')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
