d9763b69c097eb3e77f3a08560a99cd9def2a070
Ruthlessness <Nwaforbella@gmail.com>
2023-06-30 20:45:29 -0700

    task0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-include < stdio.h >
-include < unistd.h >
-
-int _putcha(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strien(char *s);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -15,4 +11,8 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-#endif
+int _atoi(char *s);
+int _putchar(char c);
+
+
+#endif 
