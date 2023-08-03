16c6e4a616fa320d7acf1ddb7221805d0ecaf79f
Ruthlessness <Nwaforbella@gmail.com>
2023-08-02 21:04:23 -0700

    linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stddef.h>
 #include "lists.h"
 
 /**
  * _strlen - returns the length of a string.
- * @s : a character
- *
+ * @s : s is a character
  * Return: value is i
  */
 int _strlen(const char *s)
@@ -22,10 +20,9 @@ int _strlen(const char *s)
 }
 
 /**
- * add_node - To add a new node at the beginning of a list_t list.
+ * add_node - adds a new node at beginning of a list_t list.
  * @head: head of a list_t list.
  * @str: value to insert into element.
- *
  * Return: the number of nodes.
  */
 list_t *add_node(list_t **head, const char *str)
